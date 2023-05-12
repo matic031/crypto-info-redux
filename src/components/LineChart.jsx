b06ac524c05b1ac50b8aace891b47acd400dcf8f
majeric.matic@scptuj.si <majeric.matic@scptuj.si>
2023-05-12 17:39:16 +0200

    package.json update

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
+import { Chart, registerables } from 'chart.js';
 
 const { Title } = Typography;
 
